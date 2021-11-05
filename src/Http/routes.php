49615758472dd37f3f6310f1dcbbb67655281d1a
Artyom Zavrazhyn <artem.zavrazhyn@uinno.io>
2021-11-05 18:14:16 +0200

    route names

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use LaravelCentrifugo\Http\Controllers\MainController;
 
-Route::post('/subscribe', [MainController::class, 'subscribe']);
-Route::post('/connect', [MainController::class, 'connect']);
-Route::post('/refresh', [MainController::class, 'refresh']);
-Route::post('/publish', [MainController::class, 'publish']);
+Route::post('/subscribe', [MainController::class, 'subscribe'])->name('laravel_centrigufo_subscribe');
+Route::post('/connect', [MainController::class, 'connect'])->name('laravel_centrigufo_connect');
+Route::post('/publish', [MainController::class, 'publish'])->name('laravel_centrigufo_publish');
